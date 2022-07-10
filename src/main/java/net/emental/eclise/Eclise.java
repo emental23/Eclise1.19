f585dab26226c8d4d65eb3e24e60e67536451dec
ement <ementalgaming.real@gmail.com>
2022-07-10 20:43:46 +0700

    test

---
+++ src/main/java/net/emental/eclise/Eclise.java
@@ -0,0 +1,15 @@
+package net.emental.eclise;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Eclise implements ModInitializer {
+	public static final String MOD_ID = "eclise";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/emental/eclise/EcliseClient.java
@@ -0,0 +1,11 @@
+package net.emental.eclise;
+
+import net.fabricmc.api.ClientModInitializer;
+
+//test
+
+public class EcliseClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+    }
+}
